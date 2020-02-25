cd207eb7ee753b755e72b6697d75871ddf3943ed
Alessandro <ale_alessandro_ale@hotmail.com>
2020-02-25 07:39:41 -0300

    buildings controller created

--- app/controllers/buildings_controller.rb
+++ app/controllers/buildings_controller.rb
@@ -1,2 +1,17 @@
+# frozen_string_literal: true
+
 class BuildingsController < ApplicationController
+  def initialize
+    @buildings_service = Buildings.new
+  end
+
+  helper_method :redirect_to_login
+  def redirect_to_login
+    redirect_to '/users/sign_in'
+  end
+
+  helper_method :load_buildings
+  def load_buildings(page = 1)
+    @buildings_service.load_page(page)
+  end
 end
