a0e7788a66373a4e9ec2e5bfee38e6baa5bf63af
Alessandro <ale_alessandro_ale@hotmail.com>
2020-02-25 07:39:10 -0300

    api connection service created

---
+++ app/services/api_connection.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+require 'httparty'
+
+class ApiConnection
+  include HTTParty
+
+  def response_buildings(page = 1)
+    bearer_token = "<ADD BEARER TOKEN HERE>"
+
+    response = HTTParty.get(
+      "https://www.orulo.com.br/api/v2/buildings", 
+      headers: {
+        "Authorization" => "Bearer #{bearer_token}",
+        "Accept" => "application/json"
+      },
+      query: {
+        page: page
+      }
+    )
+  end
+end
